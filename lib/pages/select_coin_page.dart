781681545d1c8bc64d4da30a7b726c17492a6ab0
kamaldeen121 <kamaldeenmusbaudeen48@gmail.com>
2025-03-21 06:24:28 -0700

    update in selectcoin page

--- lib/pages/select_coin_page.dart
+++ lib/pages/select_coin_page.dart
@@ -33,6 +33,17 @@ class _SelectCoinPageState extends State<SelectCoinPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          color: Colors.black,
+        ),
+      ),
       backgroundColor: Colors.white,
       body: SafeArea(
         child: Column(
